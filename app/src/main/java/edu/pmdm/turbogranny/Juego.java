042e03133cd41ca2095ebad78a5d6e031fd17262
Daniel <daniel.monforteibias@gmail.com>
2025-02-22 16:57:47 +0100

    Juego class created
    The Juego class contains the methods to render and update. For now, it paints the map

---
+++ app/src/main/java/edu/pmdm/turbogranny/Juego.java
@@ -0,0 +1,98 @@
+package edu.pmdm.turbogranny;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
+
+public class Juego extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+    private SurfaceHolder holder; //Controla surfaceview para manejar el dibujo en pantalla
+    private AppCompatActivity context;
+    //MAPA
+    public int maxX = 0, maxY = 0;
+    private Bitmap mapa;
+    private int mapHeight, mapWidth, destMapY;
+    private float velMapa = 80f;
+    private int posInicialMapa = 0, posMapaX = 0, posMapaY = 0;
+    private int frameCount = 0;
+    private static final int textoInicialX = 50;
+    private static final int textoInicialY = 20;
+
+    private BucleJuego bucleJuego;
+
+    public Juego(AppCompatActivity context) {
+        super(context);
+        holder = getHolder();
+        holder.addCallback(this);
+        this.context = context;
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        getHolder().addCallback(this);
+        Canvas canvas = this.holder.lockCanvas();
+        maxX = canvas.getWidth();
+        maxY = canvas.getHeight();
+        this.holder.unlockCanvasAndPost(canvas);
+
+        mapa = BitmapFactory.decodeResource(getResources(), R.drawable.road);
+        mapHeight = mapa.getHeight();
+        mapWidth = mapa.getWidth();
+        mapa = Bitmap.createScaledBitmap(mapa, maxX, (int) ((float) maxX / mapWidth * mapHeight), true);
+        mapHeight = mapa.getHeight(); // Actualizar el nuevo alto después de la escala
+        mapWidth = maxX; // Ancho ahora es el de la pantalla
+        posMapaY = -mapHeight + maxY;
+
+        //Creamos el Gameloop
+        bucleJuego = new BucleJuego(getHolder(), this);
+        setFocusable(true);
+        setOnTouchListener(this);
+        bucleJuego.start();
+    }
+
+    public void render(Canvas canvas) {
+        if (canvas != null) {
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawColor(Color.RED);
+            canvas.drawBitmap(mapa, posMapaX, posMapaY, null);
+
+            paint.setStyle(Paint.Style.FILL);
+            paint.setTextSize(40);
+            Rect textBounds = new Rect();
+            paint.getTextBounds("Frames ejecutados", 0, 1, textBounds);
+            canvas.drawText("Frames ejecutados: " + frameCount, textoInicialX, textoInicialY + textBounds.height(), paint);
+
+        }
+    }
+
+    public void actualizar() {
+        frameCount++;
+    }
+
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+}
